2bc7c0d8deb8b6127ae0f14c75c541c1fd7b5da8
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-11-21 16:42:55 +0700

    add

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Route::post('/add_product', [FoodController::class, 'add_product']);
+// Route::get('/get_all_product', [FoodController::class, 'get_all_product']);
+// Route::get('/get_edit_product/{id}', [FoodController::class, 'get_edit_product']);
+// Route::post('/update_product/{id}', [FoodController::class, 'update_product']);
+// Route::get('/delete_product/{id}', [FoodController::class, 'delete_product']);
