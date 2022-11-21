8d2baace1c47fe913568a972d3e690b7edc6739a
ngobao2002 <bao.ngo200202@vnuk.edu.vn>
2022-11-21 20:33:16 +0700

    update data

--- database/seeders/vp_food.php
+++ database/seeders/vp_food.php
@@ -17,35 +17,57 @@ class vp_food extends Seeder
      */
     public function run()
     {
-        // DB::table('food')->insert([
-        //     [
-        //         'name' => "Com sieu mai",
-        //         'description' => "banh mi kẹp thịt heo viet nam",
-        //         'ingredient' => "Xien thit lon",
-        //         'image' => 'http://at06.chonweb.vn/wp-content/uploads/2019/08/xaxieu.jpg',
-        //         'price' => 69000,
-        //     ],
-        //     [
-        //         'name' => "Banh mi heo quay",
-        //         'description' => "banh mi kẹp thịt heo viet nam",
-        //         'ingredient' => "Xien thit lon",
-        //         'image' => 'http://at06.chonweb.vn/wp-content/uploads/2019/08/no-cook-chicken-banh-mi_1980x1320-119440-1-150x150.jpg',
-        //         'price' => 69000,
-        //     ]
-        // ]);
-        $faker = Faker::create();
+        DB::table('food')->insert([
+            [
+                'name' => "Com sieu mai",
+                'description' => "banh mi kẹp thịt heo viet nam",
+                'price' => 69000,
+                'photo' => 'http://at06.chonweb.vn/wp-content/uploads/2019/08/xaxieu.jpg',
+                'type' => "Com",
+                'ingredient' => "Xien thit lon",
+            ],
+            [
+                'name' => "Banh mi heo quay",
+                'description' => "banh mi kẹp thịt heo viet nam",
+                'price' => 15000,
+                'photo' => 'http://at06.chonweb.vn/wp-content/uploads/2019/08/xaxieu.jpg',
+                'type' => "banh mi",
+                'ingredient' => "Xien thit lon",
+            ],
+            [
+                'name' => "com ga",
+                'description' => "banh mi kẹp thịt heo viet nam",
+                'price' => 15000,
+                'photo' => 'http://at06.chonweb.vn/wp-content/uploads/2019/08/xaxieu.jpg',
+                'type' => "com",
+                'ingredient' => "Xien thit lon",
+            ],
+            [
+                'name' => "Banh mi cha",
+                'description' => "banh mi kẹp thịt heo viet nam",
+                'price' => 15000,
+                'photo' => 'http://at06.chonweb.vn/wp-content/uploads/2019/08/xaxieu.jpg',
+                'type' => "banh mi",
+                'ingredient' => "Xien thit lon",
+            ]
+        ]);
+        // $faker = Faker::create();
 
-        for ($i = 0; $i <10; $i++){
-            DB::table('food')->insert([
-                    [
-                        'name' => $faker->name,
-                        'description' => $faker->realText,
-                        'ingredient' => $faker->text,
-                        'image' => 'http://at06.chonweb.vn/wp-content/uploads/2019/08/xaxieu.jpg',
-                        'price' => $faker->randomNumber(),
-                    ],
+        // for ($i = 0; $i <10; $i++){
+        //     DB::table('food')->insert([
+        //             [
+        //                 'name' => $faker->name,
+        //                 'description' => $faker->realText,
+        //                 'price' => $faker->randomNumber(),
+        //                 'photo' => 'http://at06.chonweb.vn/wp-content/uploads/2019/08/xaxieu.jpg',
+        //                 'type' => $faker->text,
+        //                 'ingredient' => $faker->text,
+                        
+                        
+                        
+        //             ],
                     
-                ]);
-        }
+        //         ]);
+        // }
     }
 }
